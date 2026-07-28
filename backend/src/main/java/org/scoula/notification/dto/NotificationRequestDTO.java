3213b5697e047ef8c60104a4f9f271e0f7aec832
jimmy@111percent.net <jimmy@111percent.net>
2026-07-28 16:57:17 +0900

    feat(notification): 알림 CRUD 추가

---
+++ backend/src/main/java/org/scoula/notification/dto/NotificationRequestDTO.java
@@ -0,0 +1,32 @@
+package org.scoula.notification.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.scoula.common.util.Enum;
+import org.scoula.notification.domain.NotificationVO;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NotificationRequestDTO {
+
+    private int receiverId;
+    private int senderId;
+    private Enum.NotificationType notificationType;
+    private int targetId;
+
+
+    public NotificationVO toVo(){
+        return NotificationVO.builder()
+                .receiverId(receiverId)
+                .senderId(senderId)
+                .notificationType(notificationType)
+                .targetId(targetId)
+                .build();
+    }
+
+}
