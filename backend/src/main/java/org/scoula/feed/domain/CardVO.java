35fd2d830c7afa65ed4a21cef021dc87120f7f96
jimmy@111percent.net <jimmy@111percent.net>
2026-07-30 13:10:18 +0900

    fix(controller): 이미지 vo 수정

--- backend/src/main/java/org/scoula/feed/domain/CardVO.java
+++ backend/src/main/java/org/scoula/feed/domain/CardVO.java
@@ -1,8 +1,10 @@
 package org.scoula.feed.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.scoula.common.util.UploadPathName;
 
 @Data
 @NoArgsConstructor
@@ -10,4 +12,9 @@ import lombok.NoArgsConstructor;
 public class CardVO {
     private int applyId;
     private String cardImageName;
+
+    // 프론트엔드에서 사용할 url 프로퍼티
+    public String getUrl() {
+        return "/api/feeds/cardImage/" + cardImageName;
+    }
 }
