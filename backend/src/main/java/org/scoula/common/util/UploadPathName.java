dc15082b71e61c07b027a120705e09f123cec3b9
jimmy@111percent.net <jimmy@111percent.net>
2026-08-03 16:35:13 +0900

    feat(util) : uploadpath 추가

--- backend/src/main/java/org/scoula/common/util/UploadPathName.java
+++ backend/src/main/java/org/scoula/common/util/UploadPathName.java
@@ -31,4 +31,8 @@ public class UploadPathName {
     public  static  String getCardPath(){
         return getUploadPath() + "/card/";
     }
+
+    public  static  String getProfilePath(){
+        return getUploadPath() + "/profile/";
+    }
 }
