f29d44933c65161c14345a6cc1f100acbc44ce2c
jimmy@111percent.net <jimmy@111percent.net>
2026-08-07 19:27:30 +0900

    feat(fix) : 검색 에러 수정

--- backend/src/main/java/org/scoula/search/controller/SearchController.java
+++ backend/src/main/java/org/scoula/search/controller/SearchController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/member/search")
+@RequestMapping("/api/member/search")
 @RequiredArgsConstructor
 public class SearchController {
 
--- frontend/src/api/searchApi.js
+++ frontend/src/api/searchApi.js
@@ -1,10 +1,10 @@
 import api from '@/api';
 
-const BASE_URL = '/api/member';
+const BASE_URL = '/api/member/search';
 
 export default {
   async searchMember({ keyword, userId }) {
-    const { data } = await api.get(`${BASE_URL}/search`, {
+    const { data } = await api.get(`${BASE_URL}`, {
       params: {
         keyword,
         userId,
