e3274270011ca5ac4604611774c166d5b690a977
ihyoneung <lee33750519@gmail.com>
2026-08-08 09:57:31 +0900

    chore(frontend): Bootstrap Icons 스타일 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'vue-awesome-paginate/dist/style.css';
 import './assets/main.css';
 import './assets/kb-mobile.css';
