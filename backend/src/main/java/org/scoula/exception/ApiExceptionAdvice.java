4280fe5f44a0038273294542297622037435532e
ihyoneung <lee33750519@gmail.com>
2026-08-12 14:13:46 +0900

    fix(backend): API 예외 처리 개선

--- backend/src/main/java/org/scoula/exception/ApiExceptionAdvice.java
+++ backend/src/main/java/org/scoula/exception/ApiExceptionAdvice.java
@@ -1,13 +1,10 @@
 package org.scoula.exception;
 
 import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.core.config.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 @Log4j2
@@ -25,6 +22,24 @@ public class ApiExceptionAdvice {
                                 .status(error.getStatus().value())
                                 .code(error.getCode())
                                 .message(error.getMessage())
+                                .build()
+                );
+    }
+
+    // 잘못된 요청값 예외
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponseDTO> handleIllegalArgumentException(
+            IllegalArgumentException e
+    ) {
+        log.warn(e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        ErrorResponseDTO.builder()
+                                .status(HttpStatus.BAD_REQUEST.value())
+                                .code("C001")
+                                .message(e.getMessage())
                                 .build()
                 );
     }
